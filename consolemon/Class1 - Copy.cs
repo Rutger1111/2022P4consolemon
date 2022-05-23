f4984460f0e564556393fa136c3c7502e8a3e744
Rutger1111 <90682560+Rutger1111@users.noreply.github.com>
2022-05-23 09:50:12 +0200

    alles

---
+++ consolemon/Class1 - Copy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace consolemon
+{
+    class Skills
+    {
+        internal int Damage = 10;
+        internal int EnergyCost = 1;
+        internal string Naam = "chorizord";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        internal void UseOn(Consolemon target, Consolemon caster)
+        {
+            target.TakeDamage(Damage);
+            caster.depleteEnergy(EnergyCost);
+        }
+    }
+}
+
---
+++ consolemon/Skils.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace consolemon
+{
+    class Consolemon
+    {
+        internal int Health = 100;
+        internal int Energy = 10;
+        internal string Naam = "chorizord";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+
+
+        internal void TakeDamage(int damage)
+        {
+            Health -= damage;
+        }
+
+
+
+        internal void depleteEnergy(int Energycost)
+        {
+            Energy -= Energycost;
+        }
+    }
+}
